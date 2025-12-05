9fb2a17afdb315b15441d8d3d2df7d5acc632d97
fputignani <fputignani@gmail.com>
2025-12-05 20:29:58 +0100

    Migrated to 5.6.1

--- Oraculo_ECI/Source/Oraculo_ECI/Oraculo_ECI.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Oraculo_ECI.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Oraculo_ECI, "Oraculo_ECI" );
